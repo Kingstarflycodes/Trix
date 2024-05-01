c725fb84a511eda7f0975924fef0bb24d8bfefc8
CosMo578 <raphaelakpor00@gmail.com>
2024-05-01 13:38:33 +0100

    Removed comments

--- src/pages/script.js
+++ src/pages/script.js
@@ -2,7 +2,6 @@
 let countDate = new Date('2024-11-27 00:00:00').getTime();
 
 let x = setInterval(() => {
-  //   let monthsHolder = document.querySelector('.months');
   let daysHolder = document.querySelector('.days');
   let hoursHolder = document.querySelector('.hours');
   let minutesHolder = document.querySelector('.minutes');
@@ -11,14 +10,11 @@ let x = setInterval(() => {
   let now = new Date().getTime();
   let duration = countDate - now;
 
-  //   let months = Math.floor(duration / (1000 * 60 * 60 * 24));
-
   let days = Math.floor(duration / (1000 * 60 * 60 * 24));
   let hours = Math.floor((duration % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   let minutes = Math.floor((duration % (1000 * 60 * 60)) / (1000 * 60));
   let seconds = Math.floor((duration % (1000 * 60)) / 1000);
 
-  //   monthsHolder.textContent = `${months}`;
   daysHolder.textContent = `${days}`;
   hoursHolder.textContent = `${hours} `;
   minutesHolder.textContent = `${minutes}`;
@@ -26,7 +22,6 @@ let x = setInterval(() => {
 
   if (duration < 0) {
     clearInterval(x);
-    // monthsHolder.textContent = `00`;
     daysHolder.textContent = `00`;
     hoursHolder.textContent = `00`;
     minutesHolder.textContent = `00`;
