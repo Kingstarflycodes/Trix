1dcb59f93bfea7cb5ca05da76e7c308b8670c833
Kelvin Feh <fehkelvink@gmail.com>
2024-05-13 05:36:49 +0100

    Adding main.go

---
+++ go.mod
@@ -0,0 +1,3 @@
+module Trix
+
+go 1.22.2
---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+)
+
+func main() {
+	// Parse HTML template
+	tmpl := template.Must(template.ParseFiles("templates/index.html"))
+
+	// Define a handler function to render the template
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		tmpl.Execute(w, nil)
+	})
+
+	// Start the HTTP server
+	http.ListenAndServe(":8080", nil)
+}
