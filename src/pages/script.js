d68b1d69923e3d92769eaf3f27df3e59aa146829
Tony Oloriogun <webdevloper186@gmail.com>
2024-05-13 16:18:12 +0100

    Updated Reward page

--- src/pages/script.js
+++ src/pages/script.js
@@ -28,3 +28,20 @@ let x = setInterval(() => {
     secondsHolder.textContent = `00`;
   }
 }, 1000);
+
+
+//copy function
+function Copy() {
+  // Get the text field
+  var copyText = document.getElementById("myInput");
+
+  // Select the text field
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); // For mobile devices
+
+   // Copy the text inside the text field
+  navigator.clipboard.writeText(copyText.value);
+
+  // Alert the copied text
+  alert("Id copied: " + copyText.value);
+} 
