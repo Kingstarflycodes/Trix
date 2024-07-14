0083446092d623e9be737bcffe38314b0d472eed
CosMo578 <raphaelakpor00@gmail.com>
2024-07-14 11:20:47 +0100

    Made some changes

--- waitlist/script.js
+++ waitlist/script.js
@@ -1,19 +1,31 @@
-let notCoin = document.querySelector(".notcoin");
-let ton = document.querySelector(".ton");
-let voteScroller = document.querySelector("#vote");
-let voteCount = document.querySelector(".voteCount");
-let initialVoteCount = 50;
+// let voteScroller = document.querySelector('#vote');
+// let voteCount = document.querySelector('.voteCount');
+// let initialVoteCount = 50;
+// (async function (params) {
+//     let currentVal = voteScroller.nodeValue
+//     voteCount.innerHTML = await initialVoteCount
 
-(async function (params) {
-    let currentVal = voteScroller.nodeValue
-    voteCount.innerHTML = await initialVoteCount
-    
-})()
+// })()
 
-notCoin.addEventListener("click", () => {
-    voteCount--
+let notCoin = document.querySelector('.notCoin');
+let ton = document.querySelector('.ton');
+let tonPlaceHolder = document.querySelector('.tonVal');
+let notCoinPlaceHolder = document.querySelector('.notCoinVal');
+let [notCoinVal, tonVal] = [50, 50];
+
+tonPlaceHolder.innerHTML = tonVal;
+notCoinPlaceHolder.innerHTML = notCoinVal;
+
+notCoin.addEventListener('click', () => {
+  tonVal--;
+  notCoinVal++;
+  tonPlaceHolder.innerHTML = tonVal;
+  notCoinPlaceHolder.innerHTML = notCoinVal;
 });
 
-ton.addEventListener("click", () => {
-    voteCount++
+ton.addEventListener('click', () => {
+  tonVal++;
+  notCoinVal--;
+  tonPlaceHolder.innerHTML = tonVal;
+  notCoinPlaceHolder.innerHTML = notCoinVal;
 });
