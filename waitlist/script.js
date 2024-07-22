97700762bfba192c711bd535deef52deff48191a
Dev. Ralph <raphaelakpor00@gmail.com>
2024-07-22 20:48:11 +0100

    Updated vote page (still incomplete)

--- waitlist/script.js
+++ waitlist/script.js
@@ -13,19 +13,30 @@ let tonPlaceHolder = document.querySelector('.tonVal');
 let notCoinPlaceHolder = document.querySelector('.notCoinVal');
 let [notCoinVal, tonVal] = [50, 50];
 
-tonPlaceHolder.innerHTML = tonVal;
-notCoinPlaceHolder.innerHTML = notCoinVal;
-
-notCoin.addEventListener('click', () => {
-  tonVal--;
-  notCoinVal++;
-  tonPlaceHolder.innerHTML = tonVal;
-  notCoinPlaceHolder.innerHTML = notCoinVal;
-});
+tonPlaceHolder.innerHTML = `${tonVal}%`;
+notCoinPlaceHolder.innerHTML = `${notCoinVal}%`;
 
 ton.addEventListener('click', () => {
+  if (!notCoinVal > 0) {
+    alert('Cannot decrease NotCoin Value!!');
+    return;
+  }
   tonVal++;
   notCoinVal--;
-  tonPlaceHolder.innerHTML = tonVal;
-  notCoinPlaceHolder.innerHTML = notCoinVal;
+  tonPlaceHolder.innerHTML = `${tonVal}%`;
+  tonPlaceHolder.style.width = `${tonVal}%`;
+  notCoinPlaceHolder.innerHTML = `${notCoinVal}%`;
+});
+
+notCoin.addEventListener('click', () => {
+  if (!tonVal > 0) {
+    alert('Cannot decrease Ton Value!!');
+    return;
+  }
+  notCoinVal++;
+  tonVal--;
+  tonPlaceHolder.innerHTML = `${tonVal}%`;
+  tonPlaceHolder.style.width = `${tonVal}%`;
+  notCoinPlaceHolder.innerHTML = `${notCoinVal}%`;
+  notCoinPlaceHolder.style.width = `${notCoinVal}%`;
 });
